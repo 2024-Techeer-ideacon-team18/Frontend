3448500f7cde3049e108474bae1e245163a8c858
GirlsLikeMyY <psr2560@naver.com>
2024-06-01 07:11:36 +0900

    resultPage 퍼블리싱 중

--- hackerthon18/src/component/Game.tsx
+++ hackerthon18/src/component/Game.tsx
@@ -1,48 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import backgroundImg from '../assets/Background.png';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import backgroundImg from "../assets/Background.png";
 
 //Styled Component
 const AppContainer = styled.div`
-    width: 100vw;
-    height: 100%;
-    display: flex;
-    background-image: url(${backgroundImg});
-    background-size: cover;
-    background-repeat: no-repeat;
-    min-height: 100vh;
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 100vh;
 `;
 
-const Header = styled.header`
-    
-`;
+const Header = styled.header``;
 
 const Title = styled.h1`
-    font-size: 3rem;
-    font-weight: bold;
+  font-size: 3rem;
+  font-weight: bold;
 `;
 
 const Problem = styled.p`
-    font-size: 2rem;
-    font-weight: bold;
+  font-size: 2rem;
+  font-weight: bold;
 `;
 
-const ResultBtn = styled.button`
-
-`;
+const ResultBtn = styled.button``;
 
 const Game: React.FC = () => {
-    return (
-        <AppContainer>
-            <Header>
-                <Title>open</Title>
-                <Problem>문제 : Math 함수와 관련된 용어를 고르시오</Problem>
-                <ResultBtn><Link to="/result">종료하기</Link></ResultBtn>
-            </Header>
-        </AppContainer>
-    )
-}
+  return (
+    <AppContainer>
+      <Header>
+        <Title>open</Title>
+        <Problem>문제 : Math 함수와 관련된 용어를 고르시오</Problem>
+        <ResultBtn>
+          <Link to="/result">종료하기</Link>
+        </ResultBtn>
+      </Header>
+    </AppContainer>
+  );
+};
 
 export default Game;
--- hackerthon18/src/component/Result.tsx
+++ hackerthon18/src/component/Result.tsx
@@ -1,3 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import backgroundImg from "../assets/Background.png";
+import Math from "../assets/Math.png";
+
 export default function Result() {
-    return <div></div>
+  return (
+    <AppContainer>
+      <div
+        style={{ flexDirection: "row", display: "flex", alignItems: "center" }}
+      >
+        <Title>What's your Answer?</Title>
+        <SmallContainer></SmallContainer>
+      </div>
+
+      <Container></Container>
+
+      <ResultBtn>
+        <Link to="/game">시작하기</Link>
+      </ResultBtn>
+    </AppContainer>
+  );
 }
+
+//Styled Component
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+  margin-right: 5rem;
+`;
+const Container = styled.div`
+  width: 40vw;
+  height: 40vw;
+  background: linear-gradient(
+    160deg,
+    rgba(255, 255, 255, 0.3) 0%,
+    rgba(199, 172, 255, 0.1) 100%
+  );
+  box-shadow: 0px 3.0615639686584473px 3.0615639686584473px rgba(0, 0, 0, 0.25);
+  border-radius: 28px;
+  border: 0.77px rgba(255, 255, 255, 0.5) solid;
+  backdrop-filter: blur(50px);
+`;
+
+const SmallContainer = styled.div`
+  width: 10vw;
+  height: 5vw;
+  background: linear-gradient(
+    160deg,
+    rgba(255, 255, 255, 0.3) 0%,
+    rgba(199, 172, 255, 0.1) 100%
+  );
+  box-shadow: 0px 3.0615639686584473px 3.0615639686584473px rgba(0, 0, 0, 0.25);
+  border-radius: 28px;
+  border: 0.77px rgba(255, 255, 255, 0.5) solid;
+  backdrop-filter: blur(50px);
+`;
+const ResultBtn = styled.button`
+  width: 10rem;
+  height: 3rem;
+  background-color: #fff;
+  color: #1c3487;
+  margin-top: 20px;
+  text-align: center;
+  font-weight: 200;
+`;
